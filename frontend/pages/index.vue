6901ad52842566fc8e0fbf7db3d9ff6161a58653
David Roman <roman.david@gmail.com>
2023-12-13 21:45:25 -0500

    try again

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+    const { findOne } = useStrapi()
     const msg = ref('Hello')
+
 </script>
 <template>
     <div>
         {{ msg }}
+        <hr>
+        <pre>
+            {{ findOne }}
+        </pre>
     </div>
 </template>
